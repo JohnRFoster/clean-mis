f6f13b03a1852bc433a60de7131c492eb92173e2
JohnRFoster <fosterj@bu.edu>
2024-07-11 10:39:33 -0600

    add Dog day, file paths

--- R/3.Generate.Firearms.Chronolgy.3Mar2021.R
+++ R/3.Generate.Firearms.Chronolgy.3Mar2021.R
@@ -1,10 +1,8 @@
 
-rm(list=ls()) 
+rm(list=ls())
 
-#----Set Working Dir----
-setwd("C:/DATA/MIS/PigData/Dec2020/")
-
-write.path<-"C:/Documents/Manuscripts/Feral Swine - MIS Data Description/Data/"
+#----Set Write Paths----
+write.path<-"data/processed"
 
 
 #----Load Libraries----
@@ -12,36 +10,28 @@ library(reshape2)
 library(tidyr)
 library(plyr)
 library(modeest)
+library(readr)
 library(operators)
 
-
-#----Required Functions----
-source("C:\\Documents\\Manuscripts\\Feral Swine - MIS Data Description\\Code\\FNC.MIS.Pre.Process.R")
-source("C:\\Documents\\Manuscripts\\Feral Swine - MIS Data Description\\Code\\FNC.MIS.calc.trap.effort.R")
-source("C:\\Documents\\Manuscripts\\Feral Swine - MIS Data Description\\Code\\FNC.MIS.calc.days.elapsed.R")
-source("C:\\Documents\\Manuscripts\\Feral Swine - MIS Data Description\\Code\\FNC.MIS.calc.trap.chronology.R")
-source("C:\\Documents\\Manuscripts\\Feral Swine - MIS Data Description\\Code\\FNC.Misc.Utilities.R")
-source("C:\\Documents\\Manuscripts\\Feral Swine - MIS Data Description\\Code\\FNC.MIS.assign.orphen.events.R")
-source("C:\\Documents\\Manuscripts\\Feral Swine - MIS Data Description\\Code\\FNC.MIS.merge.trap.events.R")
-
+#----Required Functions
+source("R/FNC.MIS.Pre.Process.R")
+source("R/FNC.MIS.calc.trap.effort.R")
+source("R/FNC.MIS.calc.days.elapsed.R")
+source("R/FNC.MIS.calc.trap.chronology.R")
+source("R/FNC.Misc.Utilities.R")
 
 #----Prep Data ----
 
+#----get correct data pull----
+pull.date <- config::get("pull.date")
+
 # Read data
-dat.Agr<-read.csv("processed.PropertyPull7Jan2021.csv",stringsAsFactors=FALSE)
+dat.Agr <- read_csv(file.path(write.path, paste0("processed_fs_national_property_", pull.date, ".csv")))
+dat.Kill <- read_csv(file.path(write.path, paste0("processed_fs_national_take_by_method_", pull.date, ".csv")))
+dat.Eff <- read_csv(file.path(write.path, paste0("processed_fs_national_effort_", pull.date, ".csv")))
+dat.PropKill <- read_csv(file.path(write.path, paste0("processed_fs_national_take_by_property_", pull.date, ".csv")))
+lut.property.acres <- read_csv(file.path(write.path, "processed_lut_property_acres.csv"))
 
-dat.Kill<-read.csv("processed.PigTakePropMeth16Dec2020.csv",stringsAsFactors=FALSE)
-
-dat.Eff<-read.csv("processed.Effort16Dec2020.csv",stringsAsFactors=FALSE)
-
-dat.PropKill<-read.csv("processed.PigTakeByProperty16Dec2020.csv",stringsAsFactors=FALSE)
-
-lut.property.acres<-read.csv("processed.lut.property.acres.csv",stringsAsFactors=FALSE)
-
-# Convert Dates to R Dates
-dat.Kill$WT_WORK_DATE <- as.Date(as.character(dat.Kill$WT_WORK_DATE),"%Y-%m-%d")
-dat.Eff$WT_WORK_DATE <- as.Date(as.character(dat.Eff$WT_WORK_DATE),"%Y-%m-%d")
-dat.PropKill$WT_WORK_DATE <- as.Date(as.character(dat.PropKill$WT_WORK_DATE),"%Y-%m-%d")
 
 ##----END DATA PREP----
 
@@ -69,7 +59,7 @@ plyr::count(tmp$CMP_NAME)
 
 cmp.name <- c("SPOTLIGHT","CALLING DEVICE, MANUAL(HAND,BLOWN)","NIGHT VISION/INFRARED EQUIPMENT",
                "CALLING DEVICE, ELECTRONIC","BAIT STATION","MONITORING CAMERA","CAR/TRUCK","TELEMETRY EQUIPMENT",
-               "BAIT STATION") 
+               "BAIT STATION")
 
 event.vec<-tmp[tmp$CMP_NAME %in% cmp.name,"event.id"]
 
@@ -90,16 +80,17 @@ firearms.associated<-firearms.associated[firearms.associated$event.id %!in% even
 
 nrow(firearms.associated)
 head(firearms.associated)
-
+plyr::count(firearms.associated$CMP_NAME)
 
 #Reset CMP_NAME
 firearms.associated[firearms.associated$CMP_NAME!="FIREARMS","CMP_NAME"]<-"OTHER"
+plyr::count(firearms.associated$CMP_NAME)
 
 #Set CMP_NAME = other to 0 for counting purposes
 firearms.associated[firearms.associated$CMP_NAME=="OTHER","WTCM_QTY"]<-0
 
-#Set WTM_QTY = 0 using UOM_NAME 
-uom.name <- c("HOURS","MINUTES","ATV HOURS","DRIVE HOURS")
+#Set WTM_QTY = 0 using UOM_NAME
+uom.name <- c("HOURS","MINUTES","ATV HOURS", "ATV DAY","DRIVE HOURS", "DOG DAY")
 
 firearms.associated[firearms.associated$UOM_NAME %!in% uom.name,"WTM_QTY"]<-0
 
@@ -138,13 +129,13 @@ event.vec<-unique(tmp.dat$event.id)
 #--Loop over unique events
 for(i in 1:length(event.vec)){
   tmp<-tmp.dat[tmp.dat$event.id==event.vec[i],]
-  
+
   fire.vec<-tmp[tmp$CMP_NAME=="FIREARMS","WTM_QTY"]
-  
+
   other.vec<-tmp[tmp$CMP_NAME=="OTHER","WTM_QTY"]
-  
+
   other.vec[other.vec %in% fire.vec] <- 0
-  
+
   tmp.dat[tmp.dat$event.id==event.vec[i] & tmp.dat$CMP_NAME=="OTHER","WTM_QTY"] <- other.vec
 }#END Loop
 
@@ -154,7 +145,7 @@ for(i in 1:length(event.vec)){
 
 #-- Adjust number of firearms
 #tmp.dat<-adjust.firearm.data(tmp.dat,thershold=5)
-  
+
 
 #Aggregate time
 tmp <-aggregate(cbind(WTM_QTY)~ALWS_AGRPROP_ID+AGRP_PRP_ID+CMP_NAME+WT_WORK_DATE, data=tmp.dat, FUN=sum)
@@ -196,7 +187,7 @@ kill.by.prop<-dat.PropKill[dat.PropKill$CMP_NAME %in% c("FIREARMS"),]
 kill.by.prop<-unique(kill.by.prop[,c("ALWS_AGRPROP_ID","AGRP_PRP_ID","ST_NAME","ST_GSA_STATE_CD","CNTY_GSA_CNTY_CD","WTCM_QTY","CMP_NAME","WKR_QTY","WT_WORK_DATE")])
 kill.by.prop<-kill.by.prop[,c("AGRP_PRP_ID","ALWS_AGRPROP_ID","WT_WORK_DATE","CMP_NAME","WKR_QTY")]
 colnames(kill.by.prop)[ncol(kill.by.prop)] <- "Take"
-    
+
 #Merge trap chronology and take data
 harvest.chronology <- merge(in.dat, kill.by.prop, by=c("AGRP_PRP_ID","ALWS_AGRPROP_ID","WT_WORK_DATE","CMP_NAME"),all.x = TRUE)
 harvest.chronology[is.na(harvest.chronology$Take),"Take"]<-0
@@ -218,32 +209,32 @@ harvest.chronology <- harvest.chronology[harvest.chronology$HOURS<24,]
 
 #--Plot
 hist(harvest.chronology$Take,breaks=300,xlim=c(0,50))
-summary(harvest.chronology$Take)    
+summary(harvest.chronology$Take)
 
 plot(log(harvest.chronology$Hunt.Days),harvest.chronology$Take)
 plot(harvest.chronology$Hunt.Days,harvest.chronology$Take)
 
-    
+
 #Remove Implosible Data
 #harvest.chronology<-harvest.chronology[harvest.chronology$Take<40,]
-    
-    
+
+
 #----Merge County location data
-    
+
 #Generate final data
 final.agg.out.dat <- merge(harvest.chronology, lut.property.acres, by=c("AGRP_PRP_ID","ALWS_AGRPROP_ID"),all.x=TRUE)
 final.agg.out.dat <- final.agg.out.dat[,c("AGRP_PRP_ID","ALWS_AGRPROP_ID","WT_WORK_DATE", "ST_NAME","CNTY_NAME", "ST_GSA_STATE_CD", "CNTY_GSA_CNTY_CD","FIPS","TOTAL.LAND",
                                               "CMP_NAME","HOURS","FIREARMS","Hunt.Hours","Hunt.Days","Take")]
 final.agg.out.dat <- final.agg.out.dat[order(final.agg.out.dat$AGRP_PRP_ID, final.agg.out.dat$WT_WORK_DATE),]
 nrow(final.agg.out.dat)
-    
+
 #Limit to only those with acreage
 final.agg.out.dat <- final.agg.out.dat[final.agg.out.dat$TOTAL.LAND>0,]
 nrow(final.agg.out.dat)
 
 final.agg.out.dat<-final.agg.out.dat[is.na(final.agg.out.dat$AGRP_PRP_ID)==FALSE,]
 nrow(final.agg.out.dat)
-    
+
 #--Add unk id
 unk.prp.event.id <- paste0(final.agg.out.dat$AGRP_PRP_ID,".",seq(1,nrow(final.agg.out.dat),1))
 
@@ -251,11 +242,10 @@ unk.prp.event.id <- paste0(final.agg.out.dat$AGRP_PRP_ID,".",seq(1,nrow(final.ag
 final.agg.out.dat <- data.frame(append(final.agg.out.dat, list(unk.prp.event.id=unk.prp.event.id), after=match("ALWS_AGRPROP_ID", names(final.agg.out.dat))))
 
 #----Write Data
-write.csv(final.agg.out.dat, paste0(write.path,"feral.swine.effort.take.firearms.ALL.daily.",Sys.Date(),".csv"))
-
+write.csv(final.agg.out.dat, file.path(write.path,paste0("feral.swine.effort.take.firearms.ALL.daily.",pull.date,".csv")))
 ##----END----##
-    
-  
+
+
 
 
 
@@ -266,7 +256,7 @@ write.csv(final.agg.out.dat, paste0(write.path,"feral.swine.effort.take.firearms
 
 missing.agrp.id<-unique(harvest.chronology$AGRP_PRP_ID[harvest.chronology$AGRP_PRP_ID %!in% lut.property.acres$AGRP_PRP_ID])
 
-write.csv(missing.agrp.id,"C:/Documents/Manuscripts/Feral Swine - MIS Data Description/Data/missing.agrp.id.csv",row.names=FALSE)
+write.csv(missing.agrp.id, file.path(write.path, paste0("missing.agrp.id.", pull.date, ".csv")), row.names=FALSE)
 
 
 
